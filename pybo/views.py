ef88a449339e3a36a24bfb9995657afa5743fb37
minu <anomie7777@gmail.com>
2023-01-16 00:11:09 +0900

    수동 form 작성 실습

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 from pybo.models import Question
 
 
@@ -17,9 +18,22 @@ def detail(request, question_id):
 
 
 def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), created_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.created_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
