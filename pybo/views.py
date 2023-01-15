68c34dace00ff75c6ad7f6ed337fa74f10333809
minu <anomie7777@gmail.com>
2023-01-15 23:39:30 +0900

    form 실습

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
+from .forms import QuestionForm
 from pybo.models import Question
 
 
@@ -20,3 +20,16 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), created_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.created_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
